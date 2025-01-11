f0ceffd70dda42872d75707919e372971b5464bd
Shirishreddybandi <shirishreddybandi@gmail.com>
2025-01-10 23:28:44 -0600

    rename file name

--- src/main/java/org/example/taskmanagementsystem/TaskManagementSystemApplication.java
+++ src/main/java/org/example/taskmanagementsystem/TaskManagementSystemApplication.java
@@ -1,13 +1,13 @@
-package org.example.taskmanagmentsystem;
+package org.example.taskmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskManagmentSystemApplication {
+public class TaskManagementSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TaskManagmentSystemApplication.class, args);
+        SpringApplication.run(TaskManagementSystemApplication.class, args);
     }
 
 }
--- src/test/java/org/example/taskmanagementsystem/TaskManagementSystemApplicationTests.java
+++ src/test/java/org/example/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.taskmanagmentsystem;
+package org.example.taskmanagementsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TaskManagmentSystemApplicationTests {
+class TaskManagementSystemApplicationTests {
 
     @Test
     void contextLoads() {
